4dd3049a0721f4b54c70e45d936970cd79f230fb
Aapuji <om.robotics16@gmail.com>
2025-05-06 23:55:15 -0400

    Added floats

--- src/lexer.rs
+++ src/lexer.rs
@@ -12,6 +12,7 @@ use crate::token::{self, Token, TokenKind};
 pub struct Lexer<'t> {
     //src: &'t str,
     chars: Peekable<CharIndices<'t>>,
+    ch: Option<char>, // Current char 
     pos: usize, // Holds flat index of next char, line and cols are calculated when needed
     file_id: FileId
 }
@@ -19,36 +20,49 @@ pub struct Lexer<'t> {
 impl<'t> Lexer<'t> {
     /// Creates a new `Lexer` for the main file.
     pub fn new(session: &'t mut Session) -> Self {        
-        Self {
-           // src: session.main_src(),
-            chars: session.main_src().char_indices().peekable(),
-            pos: 0,
-            file_id: *session.main_id()
-        }
+        Self::with_file_id(session, *session.main_id())
     }
 
     /// Given `file_id` must be valid.
     pub fn with_file_id(session: &'t mut Session, file_id: FileId) -> Self {
         let src = session.source_map().get(file_id).unwrap().source();
-        Self {
+        let mut chars = src.char_indices().peekable();
+        let mut ch = None;
+        if chars.peek().is_some() {
+            ch = Some(chars.next().unwrap().1);
+        }
+        
+        let mut lexer = Self {
            // src: src,
-            chars: src.char_indices().peekable(),
+            chars,
+            ch,
             pos: 0,
             file_id
-        }
+        };
+
+        // lexer.next();
+
+        lexer
     }
 
     /// Checks the current character is the last one.
     fn at_end(&mut self) -> bool {
-        self.chars.peek().is_none()
+        self.ch.is_none()
     }
 
     /// Advances iterator to next character and byte index.
     fn next(&mut self) -> Option<(usize, char)> {
+        
         if let Some((i, ch)) = self.chars.next() {
-            self.pos = i + ch.len_utf8();
+            if let Some(c) = self.ch {
+                self.pos += c.len_utf8();
+            }
+            self.ch = Some(ch);
+            
             Some((i, ch))
         } else {
+            self.ch = None;
+            
             None
         }
     }
@@ -58,8 +72,9 @@ impl<'t> Lexer<'t> {
         self.chars.peek().map(|&(_, ch)| ch)
     }
 
+    /// Skips whitespace such that when it finishes, the current character is not whitespace.
     fn skip_whitespace(&mut self) {
-        while let Some(c) = self.peek() {
+        while let Some(c) = self.ch {
             if c.is_whitespace() {
                 self.next();
             } else {
@@ -69,17 +84,17 @@ impl<'t> Lexer<'t> {
     }
 
     /// Lexes a number, given the base. If `base` is 0, then it will figure out the base.
+    /// 
+    /// Must be called only when the current character in the iterator is a digit.
     fn lex_number(&mut self, tokens: &mut Vec<Token>, mut base: u8) -> Vec<Diagnostic<FileId>> {
         let start_pos = self.pos;
-        let mut i: usize;
-        let mut ch: char;
         let mut num_kind = TokenKind::Int;
         let mut diagnostics = Vec::new();
 
         macro_rules! lex_base {
             ($n:expr) => {
                 {
-                    (i, ch) = self.next().unwrap();
+                    self.next().unwrap();
                     base = $n;
                 }
             };
@@ -87,10 +102,10 @@ impl<'t> Lexer<'t> {
 
         // If base=0, check to see if the number starts with 0_
         if base == 0 {
-            if let Some('0') = self.peek() {
-                (i, ch) = self.next().unwrap();
+            if let Some('0') = self.ch {
+                self.next();
 
-                match self.peek() {
+                match self.ch {
                     // Base 2
                     Some('b') |
                     Some('B') => lex_base!(2),
@@ -125,33 +140,72 @@ impl<'t> Lexer<'t> {
             }
         }
 
-        while let Some(c) = self.peek() {
-            if c.is_ascii_digit() || c == '_' {
-                if c >= '0' && c < ('0' as u8 + base) as char {
-                    (i, ch) = self.next().unwrap();
+        while let Some(c) = self.ch {
+            if c == '_' {
+                self.next();
+            } else if c.is_ascii_digit() {
+                if c >= '0' && c < ('0' as u8 + base.min(10)) as char {
+                    self.next();
                 } else {
                     let diagnostic = Diagnostic::error()
                         .with_message(format!("invalid digit for base {} literal", base))
-                        .with_labels(vec![
-                            Label::primary(self.file_id, self.pos..self.pos+1)
-                        ]);
+                        .with_label(Label::primary(self.file_id, self.pos..self.pos+1));
                     
                     diagnostics.push(diagnostic);
-                    break;
+                    
+                    self.next();
                 }
             } else if c.is_ascii_alphabetic() && base == 16 {
                 // Base 16 digits
                 if c >= 'a' && c <= 'f' ||
                    c >= 'A' && c <= 'F' {
-                    (i, ch) = self.next().unwrap();
+                    self.next();
                 // i for imaginary numbers
                 } else if c == 'i' {
-                    (i, ch) = self.next().unwrap();
+                    self.next();
                     num_kind = TokenKind::Imaginary;
                     break;
                 // Anything else
                 } else {
                     break;
+                }
+            } else if c == '.' {
+                if let Some(c) = self.peek() {
+                    if c.is_ascii_digit() {
+                        if base == 10 {
+                            if let TokenKind::Int = num_kind {
+                                self.next();
+                                num_kind = TokenKind::Real;
+                            } else {
+                                let diagnostic = Diagnostic::error()
+                                    .with_message("cannot have multiple floating points")
+                                    .with_label(Label::primary(self.file_id, self.pos..self.pos+1));
+
+                                diagnostics.push(diagnostic);
+
+                                self.next();
+                            }
+                        } else {
+                            let diagnostic = Diagnostic::error()
+                                .with_message(format!("base {} floating point literals are not supported", base))
+                                .with_labels(vec![
+                                    Label::primary(self.file_id, self.pos..self.pos+1)
+                                ]);
+                        
+                            diagnostics.push(diagnostic);
+                            
+                            self.next();
+                        }
+                    } else if c.is_whitespace() {
+                        self.next();
+                        num_kind = TokenKind::Int;
+                        break;
+                    } else {
+                        break;
+                    }
+                // Ends with .EOF, treat it as an int
+                } else {
+                    self.next();
                 }
             // Anything else
             } else {
@@ -199,8 +253,8 @@ impl<'t> Lexer<'t> {
         
         self.skip_whitespace();
 
-        // None if passed over a comment, and thus should repeat.
-        let res = match self.peek() {
+        // None if passed over a comment, and thus should repeat
+        let res = match self.ch {
             // Number
             Some(c) if c.is_ascii_digit() => Some(diagnostics.append(&mut self.lex_number(tokens, 0))),
             
